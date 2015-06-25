6c6f4942c5b94457a0df1f6344b28f5b4a90c74f
Mike Hatfield <oakraven13@gmail.com>
2015-06-25 20:26:15 -0300

    Implemented $ionicLoading service

--- www/app/services/eliteApi.js
+++ www/app/services/eliteApi.js
@@ -3,9 +3,9 @@
 (function () {
 	'use strict';
 
-	angular.module('eliteApp').factory('eliteApi', ['$http', '$q', eliteApi]);
+	angular.module('eliteApp').factory('eliteApi', ['$http', '$q', '$ionicLoading', eliteApi]);
 
-	function eliteApi($http, $q) {
+	function eliteApi($http, $q, $ionicLoading) {
 		var currentLeagueId;
 
 		function getLeagues() {
@@ -49,12 +49,16 @@
 		function getLeagueData() {
 			var deferred = $q.defer();
 
+			$ionicLoading.show({ template: 'Loading...' });
+
 			$http.get('http://elite-schedule.net/api/leaguedata/' + currentLeagueId)
 				.success(function (data, status) {
+				$ionicLoading.hide();
 				console.log('Received schedule data via HTTP.', data, status);
 				deferred.resolve(data);
 			})
 				.error(function () {
+				$ionicLoading.hide();
 				console.log('Error while making HTTP call.');
 				deferred.reject();
 			});
