15ccb0df66960bc9259b6fa10e910e6ab5426a35
Mike Hatfield <oakraven13@gmail.com>
2015-07-20 12:05:35 -0300

    Added map features

--- www/app/app.js
+++ www/app/app.js
@@ -1,122 +1,131 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 /// <reference path="../../typings/cordova/cordova.d.ts" />
 
-angular.module('eliteApp', ['ionic', 'angular-cache'])
+angular.module('eliteApp', ['ionic', 'angular-cache', 'uiGmapgoogle-maps'])
 
 	.run(function ($ionicPlatform, $ionicConfig, CacheFactory) {
-    
-	$ionicPlatform.ready(function () {
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		}
 
-		if (window.StatusBar) {
-			StatusBar.styleDefault();
-		}
+		$ionicPlatform.ready(function () {
+			if (window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			}
 
-		CacheFactory('leagueDataCache', { storageMode: 'localStorage', maxAge: 15 * 60 * 1000, deleteOnExpire: 'aggressive' });
-		CacheFactory('leaguesCache', { storageMode: 'localStorage', maxAge: 15 * 60 * 1000, deleteOnExpire: 'aggressive' });
-		CacheFactory('myTeamsCache', { storageMode: 'localStorage' });
-		CacheFactory('staticCache', { storageMode: 'localStorage' });
-	});
-})
+			if (window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+			CacheFactory('leagueDataCache', { storageMode: 'localStorage', maxAge: 15 * 60 * 1000, deleteOnExpire: 'aggressive' });
+			CacheFactory('leaguesCache', { storageMode: 'localStorage', maxAge: 15 * 60 * 1000, deleteOnExpire: 'aggressive' });
+			CacheFactory('myTeamsCache', { storageMode: 'localStorage' });
+			CacheFactory('staticCache', { storageMode: 'localStorage' });
+		});
+	})
 
 	.config(function ($ionicConfigProvider, $stateProvider, $urlRouterProvider, CacheFactoryProvider) {
 
-  $ionicConfigProvider.views.maxCache(0);
-    
-	angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
+		$ionicConfigProvider.views.maxCache(0);
 
-	$stateProvider
-		.state('home', {
-		abstract: true,
-		url: '/home',
-		templateUrl: 'app/home/home.html'
-	})
+		angular.extend(CacheFactoryProvider.defaults, { maxAge: 15 * 60 * 1000 });
 
-		.state('home.leagues', {
-		url: '/leagues',
-		views: {
-			'tab-leagues': {
-				templateUrl: 'app/home/leagues.html'
-			}
-		}
-	})
+		$stateProvider
+			.state('home', {
+				abstract: true,
+				url: '/home',
+				templateUrl: 'app/home/home.html'
+			})
 
-		.state('home.myteams', {
-		url: '/myteams',
-		views: {
-			'tab-myteams': {
-				templateUrl: 'app/home/myteams.html'
-			}
-		}
-	})
+			.state('home.leagues', {
+				url: '/leagues',
+				views: {
+					'tab-leagues': {
+						templateUrl: 'app/home/leagues.html'
+					}
+				}
+			})
 
-		.state('app', {
-		abstract: true,
-		url: '/app',
-		templateUrl: 'app/layout/menu-layout.html'
-	})
+			.state('home.myteams', {
+				url: '/myteams',
+				views: {
+					'tab-myteams': {
+						templateUrl: 'app/home/myteams.html'
+					}
+				}
+			})
 
-		.state('app.teams', {
-		url: '/teams',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/teams/teams.html'
-			}
-		}
-	})
+			.state('app', {
+				abstract: true,
+				url: '/app',
+				templateUrl: 'app/layout/menu-layout.html'
+			})
 
-		.state('app.team-detail', {
-		url: '/teams/:id',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/teams/team-detail.html'
-			}
-		}
-	})
+			.state('app.teams', {
+				url: '/teams',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/teams/teams.html'
+					}
+				}
+			})
 
-		.state('app.game', {
-		url: '/game/:id',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/game/game.html'
-			}
-		}
-	})
+			.state('app.team-detail', {
+				url: '/teams/:id',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/teams/team-detail.html'
+					}
+				}
+			})
 
-		.state('app.standings', {
-		url: '/standings',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/standings/standings.html'
-			}
-		}
-	})
+			.state('app.game', {
+				url: '/game/:id',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/game/game.html'
+					}
+				}
+			})
 
-		.state('app.locations', {
-		url: '/locations',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/locations/locations.html'
-			}
-		}
-	})
+			.state('app.standings', {
+				url: '/standings',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/standings/standings.html'
+					}
+				}
+			})
 
-		.state('app.rules', {
-		url: '/rules',
-		cache: false,
-		views: {
-			'mainContent': {
-				templateUrl: 'app/rules/rules.html'
-			}
-		}
+			.state('app.locations', {
+				url: '/locations',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/locations/locations.html'
+					}
+				}
+			})
+
+			.state('app.location-map', {
+				url: '/location-map/:id',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/locations/location-map.html'
+					}
+				}
+			})
+
+			.state('app.rules', {
+				url: '/rules',
+				cache: false,
+				views: {
+					'mainContent': {
+						templateUrl: 'app/rules/rules.html'
+					}
+				}
+			});
+
+		$urlRouterProvider.otherwise('/home/leagues');
 	});
-
-	$urlRouterProvider.otherwise('/home/leagues');
-});
---
+++ www/app/locations/location-map-ctrl.js
@@ -0,0 +1,45 @@
+(function () {
+	'use strict';
+
+	angular.module('eliteApp').controller('LocationMapCtrl', ['$stateParams', 'eliteApi', LocationMapCtrl]);
+
+	function LocationMapCtrl($stateParams, eliteApi) {
+		var vm = this;
+
+		vm.locationId = Number($stateParams.id);
+
+		vm.map = {
+			center: {
+				latitude: 38.897677,
+				longitude: -77.036530
+			},
+			zoom: 12,
+			options: { }
+		};
+
+		vm.marker = {}
+		
+
+		eliteApi.getLeagueData().then(function (data) {
+			vm.location = _.find(data.locations, { id: vm.locationId });
+			vm.marker = {
+				latitude: vm.location.latitude,
+				longitude: vm.location.longitude,
+				options: {
+					labelContent: vm.location.name + '<br />(Tap for directions)',	
+					labelAnchor: '10 -8',
+					labelClass: 'marker-labels'					
+				},			
+				showWindow: true
+			};
+
+			vm.map.center.latitude = vm.location.latitude;
+			vm.map.center.longitude = vm.location.longitude;
+		});
+		
+		vm.locationClicked = function(marker){
+			window.location = 'geo:' + marker.latitude + ',' + marker.longitude + ';u=35';				
+		};
+	}
+
+})();
